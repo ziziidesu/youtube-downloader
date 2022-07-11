169db1dc3d5f2214af14584c1dd79d64af57f045
jirno <jirno7prpr@gmail.com>
2022-07-12 01:38:06 +0900

    定期的にtmpファイル削除

--- server/server.ts
+++ server/server.ts
@@ -33,21 +33,55 @@ app.get('/api/youtube/:youtubeId', (req, res) => {
   const fileType = (req.query.fileType || 'mp4') as 'mp4' | 'mp3';
 
   const destFilePath = path.resolve(__dirname, `./tmp/${youtubeId}`);
-
   const url = `https://www.youtube.com/watch?v=${youtubeId}`;
 
-  // Get audio and video streams
+  // Get audio streams
   const audio = ytdl(url, { quality: 'highestaudio' })
 
+  audio.on('error', (err) => {
+    console.error(err);
+    res.status(400).send('download error!');
+  });
+
+  audio.on('end', () => {
+    console.log(`youtube file (${youtubeId}.wav) downloaded.`);
+    if (fileType === 'mp4') {
+      // mp4の場合はmp4をダウンロードして、wavとmp4をマージする
+      videoDownload();
+    }else{
+      // mp3の場合はwav -> mp3変換してから返す
+      console.log('transform mp4 -> mp3.');
+      const mp3FilePath = path.resolve(__dirname, `./tmp/${youtubeId}.mp3`);
+      exec(`ffmpeg -y -i ${destFilePath}.wav ${mp3FilePath}`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('movie translation error!');
+          return;
+        }
+        console.log(stdout);
+        console.log(stderr);
+        try {
+          fs.unlinkSync(destFilePath + `.wav`);
+        } catch (error) {
+          console.log(destFilePath + `.wav` + "を削除できませんでした");
+        }
+        res.download(mp3FilePath);
+      });
+    }
+  });
+  
   async function videoDownload() {
     const info = await ytdl.getInfo(youtubeId);
     let format = ytdl.chooseFormat(info.formats, { quality: 'highestvideo' });
     const video = ytdl(url, { filter: format => format.container === 'mp4' })
+
     video.pipe(fs.createWriteStream(destFilePath + `.mp4`));
     var starttime : number;
+
     video.once('response', () => {
       starttime = Date.now();
     });
+
     video.on('progress', (chunkLength, downloaded, total) => {
       const percent = downloaded / total;
       const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
@@ -57,43 +91,39 @@ app.get('/api/youtube/:youtubeId', (req, res) => {
       process.stdout.write(`running for: ${downloadedMinutes.toFixed(2)}minutes`);
       process.stdout.write(`, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `);
     });
+
     video.on('error', (err) => {
       console.error(err);
       res.status(400).send('download error!');
     });
+    
     video.on('end', () => {
       console.log(`youtube file (${youtubeId}.mp4) downloaded.`);
-  
-      if (fileType === 'mp4') {
-        // mp4の場合はmp4とwavをくっつける
-        console.log('merge mp4 - wav.');
-        const mergePath = destFilePath + `_encoded.mp4`;
-        exec(`ffmpeg -y -i ${destFilePath}.mp4 -i ${destFilePath}.wav -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 ${mergePath}`, (err, stdout, stderr) => {
-          if (err) {
-            console.error(err);
-            res.status(500).send('movie translation error!');
-            return;
-          }
-          console.log(stdout);
-          console.log(stderr);
-          res.download(mergePath);
-        });
-        return;
-      }else{
-        // mp3の場合はwav -> mp3変換してから返す
-        console.log('transform mp4 -> mp3.');
-        const mp3FilePath = path.resolve(__dirname, `./tmp/${youtubeId}.mp3`);
-        exec(`ffmpeg -y -i ${destFilePath}.wav ${mp3FilePath}`, (err, stdout, stderr) => {
-          if (err) {
-            console.error(err);
-            res.status(500).send('movie translation error!');
-            return;
-          }
-          console.log(stdout);
-          console.log(stderr);
-          res.download(mp3FilePath);
-        });
-      }
+
+      // mp4とwavをマージする
+      console.log('merge mp4 and wav.');
+      const mergePath = destFilePath + `_merged.mp4`;
+
+      exec(`ffmpeg -y -i ${destFilePath}.mp4 -i ${destFilePath}.wav -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 ${mergePath}`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('movie translation error!');
+          return;
+        }
+        console.log(stdout);
+        console.log(stderr);
+        try {
+          fs.unlinkSync(destFilePath + `.wav`);
+        } catch (error) {
+          console.log(destFilePath + `.wav` + "を削除できませんでした");
+        }
+        try {
+          fs.unlinkSync(destFilePath + `.mp4`);
+        } catch (error) {
+          console.log(destFilePath + `.mp4` + "を削除できませんでした");
+        }
+        res.download(mergePath);
+      });
     });
   }
 
@@ -111,18 +141,25 @@ app.get('/api/youtube/:youtubeId', (req, res) => {
     process.stdout.write(`running for: ${downloadedMinutes.toFixed(2)}minutes`);
     process.stdout.write(`, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `);
   });
-
-  audio.on('error', (err) => {
-    console.error(err);
-    res.status(400).send('download error!');
-  });
-  audio.on('end', () => {
-    console.log(`youtube file (${youtubeId}.wav) downloaded.`);
-    videoDownload();
-  });
-
-
 });
+
+async function delete_tmp(){
+  setInterval(() => {
+    const nowtime = new Date().getTime();
+    fs.readdir(path.resolve(__dirname, './tmp/'), function(err, files){
+      if (err) throw err;
+      var fileList = files.filter(function(file){
+        return fs.statSync(path.resolve(__dirname, './tmp/', file)).isFile() && /.*\_merged.mp4$/.test(file) && (nowtime - fs.statSync(path.resolve(__dirname, './tmp/', file)).mtime.getTime() > 300 * 1000); //絞り込み
+      })
+      fileList.forEach(file => {
+        fs.unlinkSync(path.resolve(__dirname, './tmp/', file));
+      });
+    });
+  }, 1000 * 60);
+}
+
+
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+delete_tmp();
